7eee75acbd6495d48592608910975b3292f925a9
cs4r <cesar.aguilera.p@gmail.com>
2015-02-01 13:47:37 +0100

    simplified largest_product method

--- python/largest-series-product/series.py
+++ python/largest-series-product/series.py
@@ -5,15 +5,7 @@ from functools import reduce
  
 def largest_product(digits, n):
 	__check_arguments(digits,n)
-	slices_list = slices(digits, n)
-	max_product = __product(slices_list[0])
-	
-	for i in slices_list[1:]:
-		i_product = __product(i)
-		if i_product > max_product:
-			max_product = i_product
-	
-	return max_product
+	return max( __product(i) for i in slices(digits, n))
 
 def __check_arguments(digits, n):
 	if not isinstance(digits, "".__class__) or not isinstance(n, int):
